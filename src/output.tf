f98ad38c350276e9e6ae3e0b496b3177a3f16964
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 12:11:59 +0200

    add ddos and nsg back in

--- src/output.tf
+++ src/output.tf
@@ -6,6 +6,18 @@ output "resource_group_location" {
   value = azurerm_resource_group.this.location
 }
 
+output "network_ddos_protection_plan_id" {
+  value = azurerm_network_ddos_protection_plan.this.id
+}
+
+output "network_ddos_protection_plan_virtual_network_ids" {
+  value = azurerm_network_ddos_protection_plan.this.virtual_network_ids
+}
+
+output "network_security_group_id" {
+  value = azurerm_network_security_group.this.id
+}
+
 # output "virtual_network_id" {
 #   value = azurerm_virtual_network.this.id
 # }
