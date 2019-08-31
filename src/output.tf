6b7b8364053a8dc571dfbda5e91537667ff1df10
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 11:57:11 +0200

    comment out everything except for resource group

--- src/output.tf
+++ src/output.tf
@@ -6,14 +6,14 @@ output "resource_group_location" {
   value = azurerm_resource_group.this.location
 }
 
-output "virtual_network_id" {
-  value = azurerm_virtual_network.this.id
-}
+# output "virtual_network_id" {
+#   value = azurerm_virtual_network.this.id
+# }
 
-output "virtual_network_name" {
-  value = azurerm_virtual_network.this.name
-}
+# output "virtual_network_name" {
+#   value = azurerm_virtual_network.this.name
+# }
 
-output "virtual_network_address_space" {
-  value = azurerm_virtual_network.this.address_space
-}
+# output "virtual_network_address_space" {
+#   value = azurerm_virtual_network.this.address_space
+# }
