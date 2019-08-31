bd6f0b37ba1941050c832b26dedb0ed41480531b
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 18:25:43 +0200

    add azure provider credentials

--- src/stage/main.tf
+++ src/stage/main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   version = "1.33.1"
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 module "network" {
