1305b72cfae72be2d10d9b32fb0f596732f6e2f1
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 12:23:07 +0200

    add network back

--- src/deploy.tf
+++ src/deploy.tf
@@ -12,34 +12,28 @@ resource "azurerm_network_security_group" "this" {
   depends_on          = [azurerm_resource_group.this]
 }
 
-# resource "azurerm_virtual_network" "this" {
-#   name                = var.virtual_network_name
-#   location            = azurerm_resource_group.this.location
-#   resource_group_name = azurerm_resource_group.this.name
-#   address_space       = ["10.0.0.0/16"]
-#   #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+resource "azurerm_virtual_network" "this" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["10.0.0.0/16"]
 
-#   ddos_protection_plan {
-#     id     = azurerm_network_ddos_protection_plan.this.id
-#     enable = true
-#   }
+  subnet {
+    name           = "azsnet_dmz"
+    address_prefix = "10.0.1.0/24"
+  }
 
-#   subnet {
-#     name           = "azsnet_dmz"
-#     address_prefix = "10.0.1.0/24"
-#   }
+  subnet {
+    name           = "azsnet_edmz"
+    address_prefix = "10.0.2.0/24"
+  }
 
-#   subnet {
-#     name           = "azsnet_edmz"
-#     address_prefix = "10.0.2.0/24"
-#   }
+  subnet {
+    name           = "azsnet_lan"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.this.id
+  }
 
-#   subnet {
-#     name           = "azsnet_lan"
-#     address_prefix = "10.0.3.0/24"
-#     security_group = azurerm_network_security_group.this.id
-#   }
-
-#   tags       = var.tags
-#   depends_on = [azurerm_resource_group.this, azurerm_network_ddos_protection_plan.this, azurerm_network_security_group.this]
-# }
+  tags       = var.tags
+  depends_on = [azurerm_resource_group.this, azurerm_network_security_group.this]
+}
--- src/output.tf
+++ src/output.tf
@@ -10,14 +10,14 @@ output "network_security_group_id" {
   value = azurerm_network_security_group.this.id
 }
 
-# output "virtual_network_id" {
-#   value = azurerm_virtual_network.this.id
-# }
+output "virtual_network_id" {
+  value = azurerm_virtual_network.this.id
+}
 
-# output "virtual_network_name" {
-#   value = azurerm_virtual_network.this.name
-# }
+output "virtual_network_name" {
+  value = azurerm_virtual_network.this.name
+}
 
-# output "virtual_network_address_space" {
-#   value = azurerm_virtual_network.this.address_space
-# }
+output "virtual_network_address_space" {
+  value = azurerm_virtual_network.this.address_space
+}
