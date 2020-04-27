9fb152295c4b036608d5d99e2ae2ac172df72fd3
Gareth van der Berg <gareth.shaun@gmail.com>
2020-04-27 10:37:46 +0200

    remove gateway

--- src/deploy.tf
+++ src/deploy.tf
@@ -15,13 +15,13 @@ module "vnet" {
   tags                    = var.tags
 }
 
-module "vngw" {
-  source = "./modules/vngw"
+# module "vngw" {
+#   source = "./modules/vngw"
 
-  gateway_name            = var.gateway_name
-  resource_group_name     = module.rg.name
-  resource_group_location = module.rg.location
-  public_ip_name          = var.public_ip_name
-  virtual_network_name    = var.virtual_network_name
-  tags                    = var.tags
-}
+#   gateway_name            = var.gateway_name
+#   resource_group_name     = module.rg.name
+#   resource_group_location = module.rg.location
+#   public_ip_name          = var.public_ip_name
+#   virtual_network_name    = var.virtual_network_name
+#   tags                    = var.tags
+# }
