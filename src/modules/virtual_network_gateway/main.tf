31a2125257b8b32033887dc9dba37f218b7ea124
Gareth van der Berg <gareth.shaun@gmail.com>
2021-03-17 17:32:50 +0200

    switch to basic

--- src/modules/virtual_network_gateway/main.tf
+++ src/modules/virtual_network_gateway/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_public_ip" "this" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
-  sku                 = "Standard"
+  sku                 = "Basic"
   tags                = var.tags
 }
 
